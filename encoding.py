0e749ad679a9ce159803b3099d8b8f4161fdd82e
hanna <hannabeasiam@gmail.com>
2016-09-23 20:24:00 -0400

    first commit

---
+++ encoding.py
@@ -0,0 +1,17 @@
+#Hanna Lee
+#Sep 16,2016
+#This program encrypted text output based on user input
+
+def main():
+    userName = userInput = encrypted = ""
+
+    userName = input("Hi, What is your name?")
+    
+    print(userName, ",Welcome to encrypted code program!\n")
+    print("This program will creates encrypted text based on shifting the letters backward by 3.")
+    print()
+    userInput = ("Are you ready to enjoy this program?, please input any sentence more then one word: ")
+
+    encrypted = userInput.upper().strinp()
+    
+    
